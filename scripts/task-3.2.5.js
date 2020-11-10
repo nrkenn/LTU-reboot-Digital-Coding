5fa991b0a02cacdb6097aa84b497243e4f3caa0b
nrkenn <72521481+nrkenn@users.noreply.github.com>
2020-11-10 21:07:52 +0000

    Task 3.2.5 js

---
+++ scripts/task-3.2.5.js
@@ -0,0 +1,17 @@
+var temperature = 50;
+
+
+if (temperature <50 ) {
+    console.log('Put on your coat');
+} else {
+    if (temperature <30) 
+        console.log('Put on your coat and a hat');
+}
+        if (temperature <0) {
+        console.log ('Stay inside');
+    }
+
+    if (temperature >50) {
+        console.log ('Just pants and vest is fine.')
+    }
+}
